63786cefbb74cb182a56381b709a9cdaba21204e
leniggo <n.eberl79@gmail.com>
2021-06-23 12:46:23 +0200

    ✨🔧 add files for the last task

--- Errors.java
+++ Errors.java
@@ -3,11 +3,18 @@
 // This class is used to generate warning and fatal error messages.
 
 class Errors {
+    static private boolean wereErrors = false;
+
     static void fatal(int lineNum, int charNum, String msg) {
         System.err.println(lineNum + ":" + charNum + " **ERROR** " + msg);
+        wereErrors = true;
     }
 
     static void warn(int lineNum, int charNum, String msg) {
         System.err.println(lineNum + ":" + charNum + " **WARNING** " + msg);
+    }
+
+    static boolean wereErrors() {
+        return wereErrors;
     }
 }
